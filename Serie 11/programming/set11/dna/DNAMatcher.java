ea100a310d4bcafdded101e66dca1598a97fdc06
tobapfel <tobapfel@hotmail.de>
2016-01-31 15:06:29 +0100

    bla

--- Serie 11/programming/set11/dna/DNAMatcher.java
+++ Serie 11/programming/set11/dna/DNAMatcher.java
@@ -15,7 +15,37 @@ public class DNAMatcher {
 	 *         DNA strings contains characters other than A, C, G, and T.
 	 */
 	public static int findFirstBindingPosition(String baseDNA, String candidateDNA) {
+		String complement = convertToComplement(candidateDNA);
+		if (baseDNA.contains(complement))
+			return -1;
+		int l = complement.length();
+		String subDNA = "";
+		while (baseDNA.length() >= complement.length()) {
+			subDNA = baseDNA.substring(0, l - 1);
+		}
 
+	}
+
+	private static String convertToComplement(String dna) {
+		String output = "";
+		char str[] = dna.toCharArray();
+		for (char letter : str) {
+			switch (letter) {
+			case 'A':
+				output += "T";
+				break;
+			case 'C':
+				output += "G";
+				break;
+			case 'G':
+				output += "G";
+				break;
+			case 'T':
+				output += "A";
+				break;
+			}
+		}
+		return output;
 	}
 
 }
