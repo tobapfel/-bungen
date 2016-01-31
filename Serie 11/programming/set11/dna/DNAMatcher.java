228c153cde6a0fd89267ae1cf0312470126cd17f
tobapfel <tobapfel@hotmail.de>
2016-01-31 15:40:15 +0100

    bla

--- Serie 11/programming/set11/dna/DNAMatcher.java
+++ Serie 11/programming/set11/dna/DNAMatcher.java
@@ -2,6 +2,10 @@ package programming.set11.dna;
 
 public class DNAMatcher {
 
+	private DNAMatcher() {
+
+	}
+
 	/**
 	 * Returns the index of the first position in baseDNA where candidateDNA can
 	 * bind to baseDNA, if any.
@@ -15,25 +19,37 @@ public class DNAMatcher {
 	 *         DNA strings contains characters other than A, C, G, and T.
 	 */
 	public static int findFirstBindingPosition(String baseDNA, String candidateDNA) {
-		if (!(checkDNA(baseDNA) && checkDNA(candidateDNA)))
+		if (!(checkDNA(baseDNA) && checkDNA(candidateDNA))) // checks if there
+															// is an unknown
+															// base
 			return -1;
 		String complement = convertToComplement(candidateDNA);
-		int l = complement.length();
-		int i = 0;
-		while (baseDNA.length() >= l) {
-			String subDNA = baseDNA.substring(0, l - 1);
-			if (subDNA.equals(complement))
+		int l = complement.length(); // string length of canidateDNA
+		int i = 0; // index
+		while (baseDNA.length() >= l) { // stops when the baseDNA is shorter
+										// then the canidateDNA
+			String subDNA = baseDNA.substring(0, l - 1); // builds a substring
+															// to be compared
+			if (subDNA.equals(complement)) // compares substring an canidateDNA
 				return i;
-			i += 1;
-			baseDNA = baseDNA.substring(1);
+			i += 1; // increase index
+			baseDNA = baseDNA.substring(1); // builds a new string without the
+											// first char of old string
 		}
 		return -1;
 	}
 
+	/**
+	 * returns the complement dna of {@code dna}
+	 * 
+	 * @param dna
+	 *            string that contains dna
+	 * @return complement dna
+	 */
 	private static String convertToComplement(String dna) {
 		String output = "";
 		char str[] = dna.toCharArray();
-		for (char letter : str) {
+		for (char letter : str) { // iterate through string
 			switch (letter) {
 			case 'A':
 				output += "T";
@@ -52,9 +68,16 @@ public class DNAMatcher {
 		return output;
 	}
 
+	/**
+	 * checks if there is an unknown base in the dna
+	 * 
+	 * @param dna
+	 *            string that contains dna
+	 * @return boolean
+	 */
 	private static boolean checkDNA(String dna) {
 		char str[] = dna.toCharArray();
-		for (char letter : str) {
+		for (char letter : str) { // iterate through string
 			if (!(letter == 'A' || letter == 'C' || letter == 'G' || letter == 'T'))
 				return false;
 		}
